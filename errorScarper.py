10a01adee6e85776adc623cc43e1c4f257540688
sanjeev kumar <sanjeev18@navgurukul.org>
2019-03-12 02:02:16 +0530

    added all project files

---
+++ errorScarper.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup as BS
+import requests,pprint,json
+
+ProgrammingLanguage = {}
+
+def Pythonerrors():
+    url = "https://docs.python.org/3/library/exceptions.html"
+    page = requests.get(url)
+    soup = BS(page.text,'html.parser')
+    ExceptionDivList = soup.find_all('div', attrs={'class':'section'})
+    allErrors = []
+    for ExceptionDiv in ExceptionDivList:
+        allExceptions = ExceptionDiv.find_all('dl')
+        for excep in allExceptions:
+            name = excep.find('dt').code.get_text().strip()
+            description = excep.find('dd').get_text().strip()
+            Error = {'errorName':'','description':'','example':'','url':[]}
+            Error['id'] = len(allErrors) + 1
+            Error['errorName']  = name
+            Error['description'] = description
+            allErrors.append(Error)
+    ProgrammingLanguage['python'] = allErrors
+    with open('data/errorCache.json','w') as file:
+        rawData = json.dumps(ProgrammingLanguage,indent=4, sort_keys=True)
+        file.write(rawData)
+        file.close()
+    return ProgrammingLanguage
+
+pythonErrors= Pythonerrors()
+pprint.pprint(pythonErrors)
