ca8fb518f52bfc6588f2508d00d3ea66c0f00378
sanjeev kumar <sanjeev18@navgurukul.org>
2019-03-12 08:00:46 +0530

    updated error handling

--- errorServer.js
+++ errorServer.js
@@ -3,30 +3,16 @@ var app = express()
 var fs = require('fs')
 app.use(express.json())
 
-var d = new Date()
-var dd = d.getDate()
-var mm = d.getMonth()
-var yy = d.getFullYear()
+var createDate = new Date()
+var month_names= ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+var TodayDate = createDate.getDate() +" "+ month_names[createDate.getMonth()] + " " + createDate.getFullYear()
 
-var TodayDate = dd.toString() + "-" + mm.toString() + "-" + yy.toString()
-
-// This api for Devlopers
-app.get('/allerrors',(req,res)=>{
-  fs.readFile( __dirname + '/errorCache.json',(err,data)=>{
-    if(err){
-      return res.json({"errorMsg":"Check your Json file"})
-    }else{
-      var allErrors = JSON.parse(data.toString)
-      return res.json(allErrors)
-    }
-  })
-})
 
 // This api for Devlopers
 app.put('/allerrors/:lang/:err',(req,res)=>{
   errorDict = req.body
   console.log(errorDict)
-  fs.readFile( __dirname + '/errorCache.json',(err,data)=>{
+  fs.readFile( __dirname + '/data/errorCache.json',(err,data)=>{
     if(err){
       return res.json({"errorMsg":"Check your Json file"})
     }else{
@@ -43,7 +29,7 @@ app.put('/allerrors/:lang/:err',(req,res)=>{
         }
       }
       var myJSON = JSON.stringify(allErrors,null,2)
-      fs.writeFile(__dirname + '/errorCache.json', myJSON, (err,data)=>{
+      fs.writeFile(__dirname + '/data/errorCache.json', myJSON, (err,data)=>{
         return res.json(error)
       })
     }
@@ -52,12 +38,25 @@ app.put('/allerrors/:lang/:err',(req,res)=>{
 })
 
 // From here all api's for users.
-app.get('/errorDetails/:lang',(req,res)=>{
-  fs.readFile( __dirname + '/errorCache.json',(err,data)=>{
+
+// Here we get all errors in diffrent language
+app.get('/errorDetails',(req,res)=>{
+  fs.readFile( __dirname + '/data/errorCache.json',(err,data)=>{
     if(err){
       return res.json({"errorMsg":"Check your Json file"})
     }else{
-      var allErrors = JSON.parse(data)
+      var allErrors = JSON.parse(data.toString())
+      return res.json(allErrors)
+    }
+  })
+})
+
+app.get('/errorDetails/:lang',(req,res)=>{
+  fs.readFile( __dirname + '/data/errorCache.json',(err,data)=>{
+    if(err){
+      return res.json({"errorMsg":"Check your Json file"})
+    }else{
+      var allErrors = JSON.parse(data.toString())
       if(allErrors.hasOwnProperty(req.params.lang)){
         return res.json(allErrors[req.params.lang])
       }
@@ -68,7 +67,7 @@ app.get('/errorDetails/:lang',(req,res)=>{
 
 app.get('/errorDetails/:lang/:err',(req,res)=>{
   var userName = req.query.username
-  fs.readFile(__dirname + "/userdetailsCache.json",(err,data)=>{
+  fs.readFile(__dirname + "/data/userdetailsCache.json",(err,data)=>{
     if(err){
       return res.json({"errorMsg":"Check your json file"})
     }
@@ -83,42 +82,40 @@ app.get('/errorDetails/:lang/:err',(req,res)=>{
       }
     }
     var myJSON = JSON.stringify(allusers,null,2)
-    fs.writeFile(__dirname + "/userdetailsCache.json",myJSON,(err,data)=>{
-      if(err){return res.json({"errorMsg":"check your json file."})}else{
-        fs.readFile(__dirname + "/errorCache.json", (err, data)=>{
-          if(err){
-            return res.json(err)
-          }else{
-            var allErrors = JSON.parse(data.toString())
-            var megaDict = {'details':'','pairs':[]}
-            for(var exception of allErrors[req.params.lang]){
-              if(exception['errorName'] == req.params.err){
-                megaDict['details'] = exception
-              }
-            }
-            fs.readFile(__dirname + "/userdetailsCache.json",(err,data)=>{
-              var allusersdict = JSON.parse(data.toString())
-              for(var user in allusersdict){
-                console.log(user)
-                if(user !== userName){
-                  for(var errorDict of allusersdict[user]){
-                    for(var errorcheck of allusersdict[userName]){
-                      if(errorDict['errorName'] == errorcheck['errorName'] && errorDict['date'] == errorcheck['date']){
-                        if(!megaDict['pairs'].includes(user)){
-                          megaDict['pairs'].push(user)
-
-                        }
-                      }
+    fs.writeFile(__dirname + "/data/userdetailsCache.json",myJSON,(err,data)=>{
+      if(err){return res.json({"errorMsg":"check your json file."})}
+    })
+    fs.readFile(__dirname + "/data/errorCache.json", (err, data)=>{
+      if(err){
+        return res.json(err)
+      }else{
+        var allErrors = JSON.parse(data.toString())
+        var megaDict = {'details':'','pairs':[]}
+        for(var exception of allErrors[req.params.lang]){
+          if(exception['errorName'] == req.params.err){
+            megaDict['details'] = exception
+          }
+        }
+      }
+      fs.readFile(__dirname + "/data/userdetailsCache.json",(err,data)=>{
+        var allusersdict = JSON.parse(data.toString())
+        for(var user in allusersdict){
+          if(user !== userName){
+            for(var errorDict of allusersdict[user]){
+              for(var errorcheck of allusersdict[userName]){
+                if(errorDict['errorName'] == errorcheck['errorName'] && errorDict['date'] == errorcheck['date']){
+                  if(!megaDict['pairs'].includes(user)){
+                    if(megaDict['pairs'].length < 5){
+                      megaDict['pairs'].push(user)
                     }
-
                   }
-                  return res.json(megaDict)
                 }
               }
-            })
+            }
           }
-        })
-      }
+        }
+        return res.json(megaDict)
+      })
     })
   })
 })
@@ -126,40 +123,3 @@ app.get('/errorDetails/:lang/:err',(req,res)=>{
 app.listen(8081,()=>{
   console.log("Server is working proper")
 })
-
-// app.get('/errorDetails/:lang/:err',(req,res)=>{
-//   var userName = req.query.username
-//   fs.readFile(__dirname + "/data/userdetailsCache.json",(err,data)=>{
-//     if(err){
-//       return res.json({"errorMsg":"Check your json file"})
-//     }
-//     else{
-//         var allusers = JSON.parse(data.toString())
-//         var errordict = {'errorName':req.params.err,'date':TodayDate}
-//         if(allusers.hasOwnProperty(userName)){
-//           allusers[userName].push(errordict)
-//         }else{
-//         allusers[userName] = []
-//         allusers[userName].push(errordict)
-//       }
-//     }
-//     var myJSON = JSON.stringify(allusers,null,2)
-//     fs.writeFile(__dirname + "/data/userdetailsCache.json",myJSON,(err,data)=>{
-//       if(err){return res.json({"errorMsg":"check your json file."})}else{
-//         fs.readFile(__dirname + "/data/errorCache.json", (err, data)=>{
-//           if(err){
-//             return res.json(err)
-//           }else{
-//             var allErrors = JSON.parse(data.toString())
-//             for(var exception of allErrors[req.params.lang]){
-//               if(exception['errorName'] == req.params.err){
-//                 return res.json(exception)
-//               }
-//             }
-//           }
-//         })
-//       }
-//     })
-//   })
-//
-// })
